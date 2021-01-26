3aebaaacbf90a95c9e745fac3e7545fe1199e0bc
egeigel7 <egeigel7@outlook.com>
2021-01-25 19:52:14 -0500

    added app config references and key vault references

--- SportsDataApp.Api/Program.cs
+++ SportsDataApp.Api/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +22,30 @@ namespace SportsDataApp.Api
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        var envName = hostingContext.HostingEnvironment.EnvironmentName;
+                        var settings = config.Build();
+                        var connection = settings.GetConnectionString("AppConfig");
+                        config.AddJsonFile("appsettings.json", true, true);
+                        var credentials = new ManagedIdentityCredential();
+                        if (envName.Equals("Development"))
+                        {
+                            config.AddJsonFile($"appsettings.{envName}.json", true, true);
+                        }
+                        else
+                        {
+                            config.AddAzureAppConfiguration(options =>
+                            {
+                                options.Connect(connection)
+                                        .ConfigureKeyVault(kv =>
+                                        {
+                                            kv.SetCredential(credentials);
+                                        });
+                            });
+                        }
+                    }
+                        );
                 });
     }
 }
